d98ba8ba43a96a7673aa4377183ae419d1312357
FaisalAlmadafei <113466869+FaisalAlmadafei@users.noreply.github.com>
2023-11-13 07:49:54 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const getReposButton = document.getElementById('get-repos');
 const reposList = document.getElementById('repos-list');
 
+
 getReposButton.addEventListener('click', async () => {
     const username = document.getElementById('username').value;
     /* Send an HTTP GET request to the GitHub /users/{username}/repos end point and 
@@ -10,6 +11,17 @@ getReposButton.addEventListener('click', async () => {
     const url = `https://api.github.com/users/${username}/repos`;
     const ulElement = document.getElementById('repos-list');
     // Fetch and display the list of repositories as li elements inside the ul element.
+     let response = await fetch(url); 
+     let data = await response.json(); 
+     data.forEach((item) => {
+        const Elem= document.createElement('li');
+        Elem.innerText= item.full_name;
+        ulElement.appendChild(Elem);
 
-});
+      });
+ 
+
+
+
+);
 
